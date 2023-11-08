2c1332b0872bc73a95ab929ac148677f197085eb
yjb23tech <yjb23tech@gmail.com>
2023-11-08 21:56:30 +0000

    Out the Mud

--- chp_11_writing_tests/test_name_function.py
+++ chp_11_writing_tests/test_name_function.py
@@ -6,8 +6,15 @@ class NamesTestCase(unittest.TestCase):
 
     def test_first_last_name(self):
         """Do names like 'Janis Joplin' work?"""
+        #This is the test below vv
         formatted_name = get_formatted_name('janis', 'joplin')
+        #The result of the test is then evaluated below vv
         self.assertEqual(formatted_name, 'Janis Joplin')
+    
+    def test_first_last_middle_name(self):
+        """Do names like 'Wolfgang Amadeus Puck' work?"""
+        formatted_name = get_formatted_name('Wolfgang', 'Amadeus', 'Puck')
+        self.assertEqual(formatted_name, 'Wolfgang Puck Amadeus')
 
 if __name__ == '__main__':
     unittest.main()
