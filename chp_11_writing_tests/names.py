e52a3671f5ca9e28f522397ac84d425f37c4f7cb
yjb23tech <yjb23tech@gmail.com>
2023-11-08 19:57:32 +0000

    Fun Fun Fun

---
+++ chp_11_writing_tests/names.py
@@ -0,0 +1,19 @@
+from name_function import get_formatted_name 
+
+practice_function = get_formatted_name("Yinka", "Banjo")
+#print(practice_function)
+
+while True:
+
+    first_name = input("\nWhat is your first name?\n")
+    if first_name == 'q':
+        break
+
+    last_name = input("\nWhat is your last name?\n")
+    if last_name == 'q':
+        break
+
+    formatted_entire_name = get_formatted_name(first_name, last_name)
+
+    print(f"\nWelcome aboard Captain {formatted_entire_name}")
+
